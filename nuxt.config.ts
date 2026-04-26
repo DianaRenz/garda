238fbeb2363a14ca0ae19c32b696b4b44f9d9595
DianaRenz <46561022+DianaRenz@users.noreply.github.com>
2026-04-26 13:26:31 +0200

    add firebase hosting (#3)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,7 @@ const image = `${url}/og-image.png`;
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
+  ssr: false,
 
   css: ["@/assets/main.scss"],
 
